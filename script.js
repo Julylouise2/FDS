e8469fe7f88936d42b2c54bebf5f047003b54694
Julylouise2 <july.louise2@gmail.com>
2025-05-17 21:49:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função que exibe uma mensagem ao clicar no botão
+document.getElementById("msgButton").addEventListener("click", function() {
+    const msg = "Cada dia que surge é uma nova oprotunidade para ser mais feliz";
+    document.getElementById("msgOutput").textContent = msg;
+   });
